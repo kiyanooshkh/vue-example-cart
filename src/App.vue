d7987e00f4e8195b2bcf66f903d7c1b5f3bf0832
Kiyanoosh Kh <khosravi.kiyanoosh@gmail.com>
2019-08-09 16:02:35 +0930

    adding example

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img width="25%" src="./assets/logo.png">
-    <HelloWorld msg="Hello Vue in CodeSandbox!" />
+    <Product :product="product" :image="image" :inventory="inventory"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+import Product from "./components/Product";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Product
+  },
+
+  data() {
+    return {
+      product: "Socks",
+      image: "./assets/vmSock-green.jpg",
+      inventory: 1
+    };
   }
 };
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/Product.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="product">
+    <div class="product-image">
+      <img :src="image" alt>
+    </div>
+    <div class="product-info">
+      <h1>{{product}}</h1>
+      <p v-if="inventory > 10">In stock</p>
+      <p v-else-if="inventory <= 1 && inventory > 0">Almost sold out</p>
+      <p v-else>Out of stock</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Product",
+  props: {
+    product: String,
+    image: String,
+    inventory: Number
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.product-image img {
+  width: 200px;
+  height: 200px;
+  background-color: red;
+}
+</style>
